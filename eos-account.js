eb1a3079d9b3df0b3396d013e583d018b6215cdf
Will in China <email.will.in.china@gmail.com>
2018-06-17 14:29:22 +0800

    minimal

--- eos-account.js
+++ eos-account.js
@@ -23,8 +23,6 @@ class EosAccount extends PolymerElement {
       <template is="dom-if" if="{{debug}}">
         <small>{{mnemonic}}</small></br>
         <small>{{seed}}</small></br>
-        <small>{{privateKey}}</small></br>
-        <small>{{publicKey}}</small></br>
         <small>{{encryptedKey}}</small></br>
         <small>{{error}}</small></br>
       </template>
@@ -47,15 +45,6 @@ class EosAccount extends PolymerElement {
       seed: {
         type: String,
       },
-      ecc: {
-        type: Object,
-      },
-      privateKey: {
-        type: String,
-      },
-      publicKey: {
-        type: String,
-      },
       encryptedKey: {
         type: Object,
       },
@@ -72,84 +61,40 @@ class EosAccount extends PolymerElement {
       "meta":{  
           "version":"5.0.3",
           "extensionId":"",
-          "websiteId":"bloxador.com",
           "lastVersion":"1.0.0"
       },
       "keychain":{  
-          "keypairs":[  
-    
-          ],
-          "identities":[  
-            // {  
-            //     "hash":"94ac218cff0d6043c4668f8b09fd32cf7d1382799d7657dd2cc7123c8924002d",
-            //     "privateKey":"{\"iv\":\"Ns72+LCJ994wFNCEno/kwA==\",\"salt\":\"snaCZLUxW2U=\",\"ct\":\"aG4MvODCp657CtX7+vRdFd+g3QppAm7MrWKflZg2Cv6MuZDiHGa/cTAo6NwdAMxvIy3umzp5zlmGfy4=\"}",
-            //     "publicKey":"EOS5RGMcX3rvGBdqANS4pBZVRGftYTz66ednfLR8mUwJpjmSGP219",
-            //     "name":"RandomRabbit2062053",
-            //     "accounts":{  
-    
-            //     },
-            //     "personal":{  
-            //       "firstname":"",
-            //       "lastname":"",
-            //       "email":"",
-            //       "birthdate":""
-            //     },
-            //     "locations":[  
-            //       {  
-            //           "name":"Unnamed Location",
-            //           "isDefault":false,
-            //           "phone":"",
-            //           "address":"",
-            //           "city":"",
-            //           "state":"",
-            //           "country":"",
-            //           "zipcode":""
-            //       }
-            //     ],
-            //     "kyc":false,
-            //     "ridl":-1
-            // }
-          ],
-          "permissions":[  
-    
-          ]
+          "keypairs":[],
+          "identities":[],
+          "permissions":[]
       },
       "settings":{  
-          "networks":[  
-    
-          ],
+          "networks":[],
           "hasEncryptionKey":true,
           "inactivityInterval":0,
           "language":"ENGLISH"
       },
-      "histories":[  
-    
-      ],
-      "hash":"94ac218cff0d6043c4668f8b09fd32cf7d1382799d7657dd2cc7123c8924002d"
+      "histories":[],
+      "hash":""
     }
 
-    // this.ecc = eosjs_ecc;
     this.$.bip39.mnemonicfromPassword(this.password)
     .then((data) => {
       let arr = JSON.parse(data)
       this.mnemonic = arr[0];
       this.seed = arr[1];
-      // this.privateKey = this.ecc.PrivateKey.fromSeed(this.seed).toWif()
-      // this.publicKey = this.ecc.PrivateKey.fromWif(this.privateKey).toPublic().toString()
-      // console.log(this.privateKey);
-      // console.log(this.publicKey);
       return this.$.aes.encrypt(this.seed, bloxAccount)
     })
     .then((data) => {
       this.encryptedKey = data;
-      this._save('bloxador', data)
+      this._download('bloxador', data)
     })
     .catch((err) => {
       this.error = err;
     })
   }
 
-  _save(name, data) {
+  _download(name, data) {
     const filename = `${name}_${+new Date()}.keychain`;
     const popup = window.document.createElement('a');
     popup.target = '_blank';
